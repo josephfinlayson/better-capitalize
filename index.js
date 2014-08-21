76a7cfd95569d3c2b90544bf60cf497ecf676e03
Joseph Finlayson <joseph.finlayson@gmail.com>
2014-08-21 14:28:32 +0100

    making it 'better'

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-module.exports = function (string) {
-  return string.charAt(0).toUpperCase() + string.substring(1);
-}
+'use strict';
+module.exports = function(string) {
+    if (string) {
+        return string.charAt(0).toUpperCase() + string.substring(1);
+    } else {
+        return false;
+    }
+};
 
-module.exports.words = function (string) {
-  return string.replace(/(^|\W)(\w)/g, function (m) {
-    return m.toUpperCase()
-  })
-}
+module.exports.words = function(string) {
+    if (string) {
+        return string.replace(/(^|\W)(\w)/g, function(m) {
+            return m.toUpperCase();
+        });
+    } else {
+        return false;
+    }
+
+};
